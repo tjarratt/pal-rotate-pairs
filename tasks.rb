12006c44c5e1b9daa4b12a72d9c944fffeabc657
Tim Jarratt <tjarratt@pivotal.io>
2019-10-15 15:39:55 +0200

    ensure pal tracker repository is clean

--- tasks.rb
+++ tasks.rb
@@ -1,5 +1,10 @@
+HOME = ENV['HOME']
+WORKSPACE = "#{HOME}/workspace"
+
 def validate_pal_tracker_clean!
-  raise 'nop'
+  Dir.chdir "#{WORKSPACE}/pal-tracker" do
+    `git diff-index --quiet HEAD --`
+    raise 'Ensure your pal-tracker git repository changes are committed and pushed' unless $? == 0
+  end
 end
 
-WORKSPACE = "#{$HOME}/workspace"
