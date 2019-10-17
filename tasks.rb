7a47ec697d10079f9544e12a20a538b7a4d354f2
Tim Jarratt <tjarratt@pivotal.io>
2019-10-17 10:05:39 +0200

    Improve error message output
    Added stubs for other functionality

--- tasks.rb
+++ tasks.rb
@@ -4,7 +4,47 @@ WORKSPACE = "#{HOME}/workspace"
 def validate_pal_tracker_clean!
   Dir.chdir "#{WORKSPACE}/pal-tracker" do
     `git diff-index --quiet HEAD --`
-    raise 'Ensure your pal-tracker git repository changes are committed and pushed' unless $? == 0
+    puts 'Wait a minute, not just yet ...'
+    puts ''
+
+    puts 'Refusing to rotate pairs while there are uncommitted changes to pal-tracker.'
+    puts ''
+
+    puts 'Resolution:'
+    puts 'Go the the pal-tracker directory, ensure your changes are committed and pushed. Then re-run this script'
+
+    exit 1
   end
 end
 
+def validate_pal_tracker_distributed_clean!
+
+end
+
+def ask_who_stays_at_workstation
+
+end
+
+def ask_who_is_joining_the_workstation
+
+end
+
+def fillin_emails_in_assignment_submission(driver, navigator)
+
+end
+
+def reset_git_author
+
+end
+
+def reset_ssh_identities
+
+end
+
+def logout_of_cloud_foundry
+
+end
+
+def verify_assignment_submission_works
+
+end
