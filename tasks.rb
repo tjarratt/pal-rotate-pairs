ff3047f48610dd9cc6df0469aaa4d6fb8b555832
Tim Jarratt <tjarratt@pivotal.io>
2019-10-24 20:06:56 +0200

    Flesh out some commands. Need to complete build.gradle rewrite

--- tasks.rb
+++ tasks.rb
@@ -30,31 +30,68 @@ def unpushed_commits?
 end
 
 def ask_who_stays_at_workstation
+  emails = read_emails_from_build_dot_gradle
 
+  puts "Who is staying at the workstation ? Please type '1' or '2'"
+  puts "1. #{emails.first}"
+  puts "2. #{emails.second}"
+  puts ''
+
+  input = gets.strip
+  puts ''
+
+  stays = case input when '1'; emails.first else emails.second end
+  puts "Okay, great then #{stays} will stay at the workstation"
+
+  return stays
+end
+
+def read_emails_from_build_dot_gradle
+  lines = File.read("#{WORKSPACE}/assignment-submission/build.gradle").split("\n")
+  email_lines = lines.map(&:lstrip).drop_while {|l| ! l.start_with?('email') }
+
+  first, second = email_lines
+    .drop_while {|l| ! l.include?('[') }
+    .take_while {|l| ! l.include?(']') }
+    .join(" ")
+    .gsub('emails = [', '')
+    .split(',')
+    .map {|email| email.gsub('"', '').strip }
+
+  return Struct.new(:first, :second).new(first, second)
 end
 
 def ask_who_is_joining_the_workstation
+  puts "... and what is the email address for the person who will be joining the workstation ?"
 
+  new_email = gets.strip
+  puts ''
+
+  return new_email
 end
 
-def fillin_emails_in_assignment_submission(driver, navigator)
+def fill_in_emails_in_assignment_submission(driver, navigator)
 
 end
 
 def reset_git_author
-
+  `git config --global --unset user.email`
+  `git config --global --unset user.name`
 end
 
 def reset_ssh_identities
-
+  `ssh-add -D`
 end
 
 def logout_of_cloud_foundry
-
+  `cf logout`
 end
 
-def verify_assignment_submission_works
-
+def verify_assignment_submission_works!
+  Dir.chdir("#{WORKSPACE}/assignment-submission") do
+    `./gradlew testAssignment -PexampleUrl=http://www.example.com`
+    Something.halt if $? != 0
+  end
 end
 
 def success!
@@ -63,10 +100,6 @@ def success!
 end
 
 class Exceptionish
-   def initialize(repo_name)
-    @repo_name = repo_name
-  end
-
   def halt
     puts 'Wait a minute, not just yet ...'
     puts ''
@@ -78,11 +111,24 @@ class Exceptionish
     puts resolution
 
     exit 1
+  end
+end
 
+class AssignmentSubmissionFailed < Exceptionish
+  def reason_for_exit
+    "There was a problem verifying that the assignment submission tool still works."
+  end
+
+  def resolution
+    "Go to the #{WORKSPACE}/assignment-submission directory, inspect the build.gradle, and run the testAssignment task to verify it is working"
   end
 end
 
 class Dirty < Exceptionish
+  def initialize(repo_name)
+    @repo_name = repo_name
+  end
+
  def reason_for_exit
     "Refusing to rotate pairs while there are uncommitted changes to #{@repo_name}."
   end
@@ -93,6 +139,10 @@ class Dirty < Exceptionish
 end
 
 class Unpushed < Exceptionish
+  def initialize(repo_name)
+    @repo_name = repo_name
+  end
+
   def reason_for_exit
     "Refusing to rotate pairs while there are unpushed changes to #{@repo_name}."
   end
